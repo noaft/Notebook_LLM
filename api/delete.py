b61419f042f2051c648dc8ef9b9d79602ce75a08
noaft <nvantoan1203@gmail.com>
2025-03-19 19:09:29 +0700

    add api for delete file

--- api/delete.py
+++ api/delete.py
@@ -1,7 +1,27 @@
 from fastapi import APIRouter
+import os
+from pydantic import BaseModel
 
 router = APIRouter()
 
-@router.post("/delete")
+@router.post("/delete_one")
 def delete(filename: str):
-    pass
+    extensions = [".pdf", ".json", ".bin"]
+    filename_all = filename.split[".pdf"]
+    for e in extensions:
+        os.remove(f"./tmp/{filename_all}" + e)
+
+    return "Success delete file"
+
+class data(BaseModel):
+    filenames: list[str]
+
+@router.post("/delete")
+def delete(data: data):
+    for filename in data.filenames:
+        extensions = [".pdf", ".json", ".bin"]
+        filename_all = filename.split[".pdf"]
+        for e in extensions:
+            os.remove(f"./tmp/{filename_all}" + e)
+
+    return "Success delete file"
